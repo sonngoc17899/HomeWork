c2518a3400e68e08f51d0e72ce85f6e049b24d4f
SonNgoc <sonngoc17899@gmail.com>
2020-02-24 18:50:58 +0700

    commit

--- ss4/bai4.js
+++ ss4/bai4.js
@@ -2,21 +2,20 @@ let a = parseInt(prompt("Nhap vao so a: "));
 let b = parseInt(prompt("Nhap vao so b: "));
 let c = parseInt(prompt("Nhap vao so c: "));
 let max, min, mid;
-if(b >= a){
-    max = b;
-    min = a;
-}else{
-    max = a;
-    min = b;
+if (b >= a) {
+  max = b;
+  min = a;
+} else {
+  max = a;
+  min = b;
 }
-
-if(c >= max){
-    mid = max;
-    max = c;   
-}else if(c <= min){
-    mid = min;
-    min = c;
-}else {
-    mid = c;
+if (c >= max) {
+  mid = max;
+  max = c;
+} else if (c <= min) {
+  mid = min;
+  min = c;
+} else {
+  mid = c;
 }
-console.log("In theo thu tu tang dan "+min+" "+mid+" "+max+"");
+console.log("In theo thu tu tang dan " + min + " " + mid + " " + max + "");
--- ss4/bai8.js
+++ ss4/bai8.js
@@ -1,13 +1,8 @@
-Boolean soNguyenTo(parseInt(x)){
-    if(x<2) return false;
-    else if(x>2){
-        if(x%2===0) return false;
-        for(parseInt(i=2), i<=Math.sqrt(x), i++)
-        {
-            if(x%i===0) return false;
-        }
+function soNguyenTo(parseInt(x)){
+    if(x<2){
+        console.log(");
+        
     }
-    return true;
 }
 let x = Number(prompt("Nhap vao 1 so: "));
 let sqr = parseInt(Math.sqrt(x));
