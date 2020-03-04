e8b4ef3d82451f22a5f6e0f66ec2915a77ab3f21
SonNgoc <sonngoc17899@gmail.com>
2020-03-04 17:43:41 +0700

    commit

--- ss4/bai8.js
+++ ss4/bai8.js
@@ -1,13 +1,32 @@
-function soNguyenTo(parseInt(x)){
-    if(x<2){
-        console.log(");
+// function soNguyenTo(parseInt(x)){
+//     if(x<2){
+//         console.log(");
         
-    }
+//     }
+// }
+// let x = Number(prompt("Nhap vao 1 so: "));
+// let sqr = parseInt(Math.sqrt(x));
+// if(sqr*sqr === x){
+//     console.log("So "+x+" la so chinh phuong!"); 
+// }
+let s = 20000;
+let j = 0;
+for(let i=0;i<9;i++)
+{
+    s *=2;
+    j+=s;
 }
-let x = Number(prompt("Nhap vao 1 so: "));
-let sqr = parseInt(Math.sqrt(x));
-if(sqr*sqr === x){
-    console.log("So "+x+" la so chinh phuong!"); 
-}
+console.log(s);
+console.log(j);
+
+console.log(s*2);
+console.log(j+s*2);
+
+
+
+
+
+
+
 
 
--- ss6/string.js
+++ ss6/string.js
@@ -2,10 +2,12 @@ let a = ["Javascript", "Html", "Css", "Pascal"];
 let key = prompt("Nhap mot ki tu: ");
 for(let i=0;i<a.length;i++)
 {
-    if(a[i].indexOf(key)>=0)
+    if(a[i].indexOf(key)>=0 || a[i].indexOf(key.toLowerCase)>=0)
     {
         console.log(a[i]);        
     }
 }
 //liet ke cac chu a, e, o, i, y, u
+// so sanh 2 ten
+// dau tien chuyen het ve cung 1 kieu
 
